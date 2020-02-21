6cd5024828919502e056b11cf4bbf0b0d6f467d4
YifanShenSZ <yshen57@jhu.edu>
2020-02-20 19:32:33 -0500

    Remove unnecessary argument

--- FortranLibrary/General.py
+++ FortranLibrary/General.py
@@ -2,7 +2,7 @@ from .basic import *
 
 ShowTime = FL.general_mp_showtime_
 
-def dScientificNotation(x:float, i:int) -> (float, int):
-    x_copy = c_double(x); i_copy = c_int(i)
-    FL.general_mp_dscientificnotation_(byref(x_copy),byref(i_copy))
-    return x_copy.value, i_copy.value
+def dScientificNotation(x:float) -> (float, int):
+    x_copy = c_double(x); i = c_int(0)
+    FL.general_mp_dscientificnotation_(byref(x_copy),byref(i))
+    return x_copy.value, i.value
